30e181b3a58373601d2cf63864dd2e7953cc3aec
Motohiro Takayama <mootoh@gmail.com>
2020-01-03 02:37:18 +0900

    non-converting String version but I guess it's not optimal

--- 9-PalindromeNumber.kt
+++ 9-PalindromeNumber.kt
@@ -1,5 +1,5 @@
 class Solution {
-    fun isPalindrome(x: Int): Boolean {
+    fun isPalindrome_withString(x: Int): Boolean {
         val xs = x.toString()
         var i=0
         var j=xs.length-1
@@ -9,6 +9,31 @@ class Solution {
             i += 1
             j -= 1
         }
+        return true
+    }
+
+    fun isPalindrome(x: Int): Boolean {
+        if (x < 0)
+            return false
+        
+        val stack = mutableListOf<Int>()
+        var t = x
+        while (t != 0) {
+            val y = t % 10
+            stack.add(y)
+            t = t / 10
+        }
+
+        var i=0
+        var j=stack.size-1
+        while (i < j) {
+            if (stack[i] != stack[j])
+                return false
+            i += 1
+            j -= 1
+        }
+        
+
         return true
     }
 }
