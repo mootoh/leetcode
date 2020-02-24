966479b89dabad6bf7954ea6559451f2a8914824
Motohiro Takayama <mootoh@gmail.com>
2020-02-24 14:56:14 +0900

    Simple binary search but had two pitfalls

---
+++ 774-FirstBadVersion.cc
@@ -0,0 +1,41 @@
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version) {
+    switch (version) {
+        case 1:
+        case 2:
+        case 3:
+             return false;
+        case 4:
+        default: return true;
+    }
+}
+
+class Solution {
+public:
+    // n is the latest and broken version
+    int firstBadVersion(int n) {
+        int good = 1;
+        int bad = n;
+        while (good < bad) {
+            int cur = (int)(((long long)good+(long long)bad)/2LL);
+            if (isBadVersion(cur)) {
+                if (bad == cur)
+                    return bad;
+                bad = cur;
+            } else {
+                if (good == cur)
+                    return bad;
+                good = cur;
+            }
+        }
+        return 1;
+    }
+};
+
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv) {
+    cout << (new Solution())->firstBadVersion(5) << endl;
+    cout << (new Solution())->firstBadVersion(4) << endl;
+    return 0;
+}
